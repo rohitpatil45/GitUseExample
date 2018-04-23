a138a0ec39bf98cd21c72bdaa1c3e3dfc8ac7140
Evalite <evalite@m928435.theaa.local>
2018-04-23 13:43:23 +0100

    Added map_feature branch

---
+++ MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Evalite on 23/04/2018.
+//  Copyright © 2018 RP. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
